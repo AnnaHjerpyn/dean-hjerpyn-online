59555982dd1c304e50d5cec5639a89af1b246302
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2026-07-13 13:23:08 -0400

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,25 @@
-import { type SchemaTypeDefinition } from "sanity";
+import type { SchemaTypeDefinition } from "sanity";
+
 import { projectType } from "./project";
 import { siteSettingsType } from "./siteSettings";
 import { fieldJournalEntryType } from "./fieldJournalEntryType";
 
+import {
+  awardEntryType,
+  cvPageType,
+  educationEntryType,
+  experienceEntryType,
+} from "./cvPage";
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [projectType, siteSettingsType, fieldJournalEntryType],
+  types: [
+    projectType,
+    siteSettingsType,
+    fieldJournalEntryType,
+
+    educationEntryType,
+    experienceEntryType,
+    awardEntryType,
+    cvPageType,
+  ],
 };
