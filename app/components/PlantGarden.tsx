d84451db7201f6a14910eb0204f46d9451f64466
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2026-08-27 08:53:47 -0400

    up it

--- app/components/PlantGarden.tsx
+++ app/components/PlantGarden.tsx
@@ -25,7 +25,7 @@ type PlantPlacement = {
 
 const MOBILE_BREAKPOINT = 768;
 
-const DESKTOP_PLANT_COUNT = 120;
+const DESKTOP_PLANT_COUNT = 130;
 const MOBILE_PLANT_COUNT = 55;
 
 const DESKTOP_COLUMNS = 20;
